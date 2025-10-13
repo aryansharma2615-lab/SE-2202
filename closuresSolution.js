5718e56bc65bb8711a63e2e97b3f1e7de9367e7c
aryansharma2615-lab <aryan.sharma2615@gmail.com>
2025-10-13 19:59:25 -0400

    Update closuresSolution.js

--- closuresSolution.js
+++ closuresSolution.js
@@ -1,18 +1,26 @@
 let counter = function (increment) {
-    // private variable
+    /*
+        Uncomment the line that will correctly result in a separate (rather than shared) 
+        Counter if multiple counter functions are defined
+    */
+    //count = 0
     let count = 0;
 
-    // inner function (closure)
+    /*
+        Create and return an inner function that receives a value increment
+        and increments the value stored in count by increment.
+    */
     return function () {
         count += increment;
         return count;
     };
 };
 
-let countByTwo = counter(2);
-let countByOne = counter(1);
+let countByTwo = counter(2); // closure that adds two each call
+let countByOne = counter(1); // closure that adds one each call
 
-console.log(countByTwo()); // 2
-console.log(countByOne()); // 1
-console.log(countByTwo()); // 4
-console.log(countByOne()); // 2
+// DO NOT change the lines below
+console.log(countByTwo());
+console.log(countByOne());
+console.log(countByTwo());
+console.log(countByOne());
